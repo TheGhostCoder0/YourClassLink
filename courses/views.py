6fbe2199b2ac617220b6f13f3c17d4a9c74146f1
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-04-14 01:18:00 -0700

    Resolved merge conflict in courses/views.py again again

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Course, CourseForm
 from django.urls import reverse
-from utils import notification
 
 
 # Create your views here.
@@ -15,7 +14,6 @@ def add_course(request):
             form.save()
             form = CourseForm()  # clear the form
             show_modal = True  # set show_modal to True when the form is successfully submitted
-            # notification()
             notification()
 
     else:

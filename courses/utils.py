7ea787099c6bff7fbf7df00eb2aa6a2396e0bea1
TheGhostCoder0 <105943310+TheGhostCoder0@users.noreply.github.com>
2023-04-14 01:09:12 -0700

    Notify comment out

---
+++ courses/utils.py
@@ -0,0 +1,11 @@
+from smtplib import SMTP
+
+
+def notification():
+    my_email = "yourclasslink.auth@gmail.com"
+    password = "syirdlcfvjppaeuj"
+    with SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs="keenavasiloff@gmail.com",
+                            msg=f"Subject:Your Class Link Notification\n\nClass Added")
--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from .models import Course, CourseForm
 from django.urls import reverse
+from utils import notification
 
 
 # Create your views here.
@@ -14,6 +15,7 @@ def add_course(request):
             form.save()
             form = CourseForm()  # clear the form
             show_modal = True  # set show_modal to True when the form is successfully submitted
+            # notification()
 
     else:
         form = CourseForm()
